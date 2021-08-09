d2aa1c864e0da887c8d38e7c9693e9359c56f7cd
FISERVARG\mcaballaro <matias.caballaro@outlook.com>
2021-08-09 14:29:58 -0300

    test

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +13,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 @RestController
 @RequestMapping("/api")
@@ -19,9 +22,22 @@ public class SalvoController {
     @Autowired
     private GameRepository gameRepository;
 
-   // Paso 1 devuelve toda la lista de búsqueda
+
+
+     /* // DEVUELVE UN SOLO VALOR FORZADO
 
     @RequestMapping("/games")
+    public Map<String, Object> listaGameId(GameRepository gameRepository) {
+        Map<String, Object> listGameId = new LinkedHashMap<String, Object>();
+        listGameId.put("id", "1");
+        listGameId.put("id", "2");
+        return listGameId;
+    }
+    */
+
+    // Paso 1 devuelve toda la lista de búsqueda
+
+    @RequestMapping("/games-test1")
     public List<Game> getAll() {
         return gameRepository.findAll();
     }
@@ -41,29 +57,31 @@ public class SalvoController {
     }
 
 
-    /* // DEVUELVE UN SOLO VALOR FORZADO
-
-    @RequestMapping("/games")
-    public Map<String, Object> listaGameId(GameRepository gameRepository) {
-        Map<String, Object> listGameId = new LinkedHashMap<String, Object>();
-        listGameId.put("id", "1");
-        listGameId.put("id", "2");
-        return listGameId;
-    }
-    */
-
-
-
-
 
     /*
-    public List<Billing> getGroceryBills(List<Billing> billings) {
-        return
-                billings.stream()
-                        .filter(b -> b.getType() == Billing.GROCERY)
-                        .sorted((b1, b2) -> b2.getValue() - b1.getValue())
-                        .collect(toList());
+    @RequestMapping("/games")
+    public List<Object> listaGameId() {
+        return gameRepository
+                .findAll()
+                .stream()
+                .map(game -> game.getId())
+                .collect(Collectors.toList());
     }
     */
+
+
+    @RequestMapping("/games")
+    public List<Object> listaGames() {
+        return gameRepository
+                .findAll()
+                .stream()
+                .map(game -> game.getId())
+                .collect(Collectors.toList());
+    }
+
+
+
+
+
 
 }
